7924d0df8375de57f212208d6454ba63d14d8f14
kkkkoyo <mmkrz.sg@gmail.com>
2016-07-31 17:11:23 +0900

    メインを追加しました

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package araaai;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		GameTop a=new GameTop();
+		
+		System.out.println("数値入力");
+		
+		Scanner scan1=new Scanner (System.in);
+		
+		int x=scan1.nextInt();
+		
+		System.out.println("数値入力");
+		
+		
+		
+	}
+}
